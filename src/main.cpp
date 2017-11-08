bb1f3c55889b6cd982870e42ac8a5852d75ddac2
Adam Novotny <adam.novotny@postrock.wall>
2017-11-07 21:20:38 -0500

    commented and cleaned up code in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,29 @@
 
 
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); // Create the motor shield object with the default I2C address
-// Adafruit_DCMotor *rightMotor = AFMS.getMotor(3); // Select which 'port' M1, M2, M3 or M4. In this case, M1
-// Adafruit_DCMotor *leftMotor = AFMS.getMotor(4);
+
 Adafruit_StepperMotor *rightMotor = AFMS.getStepper(200, 1);
 Adafruit_StepperMotor *leftMotor = AFMS.getStepper(200, 2);
 
-// Maximum speed to run motors (1-255)
+
+// Maximum speed to run motors
 const int max_speed = 12;
 
-// Spacing between spools in mm
-int interspool_spacing = 900;
+// Width between spools in mm
+const int interspool_spacing = 900;
 
 // Conversion from mm to stepper motor steps
-// Circumference
-const float mm_to_steps = 144.0 /* Circumference in mm */ / 200 /* steps / rotation */;
+const float mm_to_steps = 144.0 /* Circumference in mm */ / 200 /* steps per rotation */;
 
+// Artboard size
+const int max_dim = 100;
+const int artboard_to_mm = interspool_spacing / max_dim;
 
-// Number of points in the path (should be done programatically)
+// Old test path code, please remove
+// Units are in width/artboard_dims mm
+const float x_scale = 1;
+const float y_scale = -1;
+
 const int num_path = 25;
 const float path[][2] =   {
   {0,0}, // Init @ origin
@@ -51,16 +57,9 @@ const float path[][2] =   {
   // {0,0}
 };
 
-// What to scale the path coordinates by
-// Values after scaling should be -.5 to .5
-const float x_scale = 1;
-const float y_scale = -1;
 
 
-int incomingByte = 0;
-
-
-// current rope lengths
+// current cable lengths
 const int init_rope_length = (interspool_spacing/sqrt(2)) * mm_to_steps;
 int cur_lengths[2] = { init_rope_length, init_rope_length };
 
@@ -71,7 +70,8 @@ int l_to_dir(float speed) {
   return RELEASE;
 }
 
-
+// TODO: Make this set the path
+// int incomingByte = 0;
 // int* getPath() {
 //   while (Serial.available() > 0) {
 //     incomingByte = Serial.read();
@@ -85,11 +85,12 @@ int l_to_dir(float speed) {
 
 void run_motors(int dl_l, int dl_r){
   /* Given dl_l and dl_r, move the motors by that much
-   * dl_l (delta length left) in units step */
+     dl_l (delta length left) in units step */
+
+  /* TODO: Rewrite to run both steppers at the same time, with their speeds modulated to make slope.
+           This will probably require acceleration for ramping up and down */
 
   if(dl_l == 0 && dl_r == 0) return;
-
-  Serial.print("run_motors ");
 
   // Init variables of remaining steps
   int l_steps = abs(dl_l);
@@ -99,14 +100,14 @@ void run_motors(int dl_l, int dl_r){
   int dl, dr;
 
   while ((l_steps > 0) || (r_steps > 0)) {
-    //
+    // Sets the smaller step to 1 and the larger step to ratio between r_steps and l_steps
     if (l_steps > r_steps){
-      dl = 1;
+      dl = max(l_steps, 1);
       dr = r_steps / l_steps;
     }
     else {
       dl = l_steps / r_steps;
-      dr = 1;
+      dr = max(r_steps, 1);
     }
 
     rightMotor->step(dl, l_to_dir(dl_r), DOUBLE);
@@ -120,7 +121,6 @@ void run_motors(int dl_l, int dl_r){
 
 void set_lengths(int len_l, int len_r){
   /* Moves the motors to set the string to the desired lengths */
-  Serial.println("setting lr" + String(len_l) + " " + String(len_r));
 
   int delta_l_l = cur_lengths[0] - len_l;
   int delta_l_r = cur_lengths[1] - len_r;
@@ -136,13 +136,8 @@ void set_position(float x, float y){
   /* Moves the marker to the position x,y
      x and y are floats between (0,1)
      Origin is the top left corner */
-  Serial.println("setting xy" + String(x) + " " + String(y));
 
-  // The length of the square
-  const int max_dim = 100;
-  const int artboard_to_mm = interspool_spacing / max_dim;
-
-  Serial.println("artboard_to_mm" + String(artboard_to_mm) + " " +mm_to_steps + " " + String(artboard_to_mm * mm_to_steps));
+  // Serial.println("setting xy" + String(x) + " " + String(y));
 
   float new_length_l = sqrt(x*x + y*y) * artboard_to_mm * mm_to_steps;
   float new_length_r = sqrt((max_dim - x)*(max_dim - x) + y*y) * artboard_to_mm * mm_to_steps;
@@ -158,21 +153,20 @@ void setup() {
 
   Serial.println("begin.");
 
-  Serial.println("init ls " + String(cur_lengths[0]));
 
+  leftMotor->setSpeed(max_speed);
+  rightMotor->setSpeed(max_speed);
 
-  leftMotor->setSpeed(12);
-  rightMotor->setSpeed(12);
-  // getPath;
+  // Run through the hard coded path
   for(int i = 0; i < num_path; i++){
     Serial.print(path[i][0]);
     Serial.print(' ');
     Serial.println(path[i][1]);
     // Lots of messy stuff here.
-    // set_position has origin in the top left, the 0.5's are there to move the origin to the center
+    // The 50's are there to move the origin of the path to the center of the board
     set_position((path[i][0] * x_scale) + 50, 50 - (path[i][1] * y_scale));
-    // Let the motors stop
-    delay(100);
+    // Let the motors rest
+    delay(10);
   }
 
   leftMotor->release();
@@ -182,5 +176,6 @@ void setup() {
 
 
 void loop() {
+  // Spin the wheels
   delay(10);
 }
