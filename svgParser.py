e0f09c82ed97ab69a9f56e560871c1331f6acfc5
Ian Paul <ian.paul@students.olin.edu>
2017-12-11 20:22:01 -0500

    documentation added

--- svgParser.py
+++ svgParser.py
@@ -4,9 +4,11 @@ from xml.dom import minidom
 import argparse
 import re # for editing the cpp
 
+# Define types for parsing SVG
 lineType = type(Line(start=(0+0j),end=(1,1j)))
 quadType = type(QuadraticBezier(start=(0+0j), control=(.5+.5j), end=(1+1j)))
 
+# Define parse for CLI
 parser = argparse.ArgumentParser(description='Take a file and scale.')
 parser.add_argument('filename')
 parser.add_argument('minx', type=int)
@@ -15,6 +17,7 @@ parser.add_argument('maxx', type=int)
 parser.add_argument('maxy', type=int)
 args = parser.parse_args()
 
+# Get the user define output box
 outBox = [args.minx,args.miny, args.maxx, args.maxy]
 
 # Use the second solution on this stack overflow post to guarandtee better results
@@ -23,36 +26,41 @@ outBox = [args.minx,args.miny, args.maxx, args.maxy]
 poesvg = open(args.filename, 'r')
 
 doc = minidom.parse(poesvg)  # parseString also exists
-viewBox = [el.getAttribute('viewBox') for el
-        in doc.getElementsByTagName('svg')]
+
+# Get the svg width and height from an SVG made using the SO toolchain
 svgWidth = [el.getAttribute('width') for el
         in doc.getElementsByTagName('rect')]
 svgHeight =[el.getAttribute('height') for el
         in doc.getElementsByTagName('rect')]
-
 doc.unlink()
+
+# Convert the svg shape into the box format
 svgWidth = int(''.join(list(filter(str.isdigit, svgWidth[0]))))
 svgHeight = int(''.join(list(filter(str.isdigit, svgHeight[0]))))
 viewBox=[0,0,svgWidth, svgHeight]
 
+# Initialize marker control variables
 currentPosition = (0,0)
 markerDown = True
 
+# Initialize string for path
 stringToReturn = ""
 stringToReturn += "//PYTHONSTARTFLAG \n"
 
 def pointScale(svgBox, outBox, point):
+    ### Scale a point from the size/shape of svg box to the size/shape of outBox
     xScale = (outBox[2]-outBox[0])/(svgBox[2]-svgBox[0])
     yScale = (outBox[3]-outBox[1])/(svgBox[3]-svgBox[1])
     return(int(point.real *xScale), int(point.imag*yScale))
 
-paths, attributes = svg2paths('PoE.svg')
+paths, attributes = svg2paths(args.filename)
 for path in paths:
     for seg in path:
 
         if currentPosition != seg.point(0):
             # If the start point of a segment is off, pen up
             stringToReturn += "{-10,0}"
+            stringToReturn += ('\n')
             markerDown = False
 
         if(type(seg)==lineType):
@@ -61,9 +69,11 @@ for path in paths:
                 stringToReturn += ("{" + str(pointToSend[0]) + "," + str(pointToSend[1]) + "}")
                 stringToReturn += ('\n')
                 currentPosition = seg.point(i)
-                if !markerDown:
+                if not(markerDown):
                     # If the marker is up, set it down
                     stringToReturn += "{-20,0}"
+                    stringToReturn += ('\n')
+                    markerDown = True
 
         if(type(seg)==quadType):
             for x in range(0,10):
@@ -71,9 +81,11 @@ for path in paths:
                 stringToReturn += ("{" + str(pointToSend[0]) + "," + str(pointToSend[1]) + "}")
                 stringToReturn += ('\n')
                 currentPosition = seg.point(i)
-                if !markerDown:
+                if not(markerDown):
                     # If the marker is up, set it down
                     stringToReturn += "{-20,0}"
+                    stringToReturn += ('\n')
+                    markerDown = True
 
 stringToReturn +="//PYTHONENDFLAG"
 
